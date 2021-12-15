4ecb0c69d15b8454492229f58b60af1b0d5b3f50
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 09:30:21 +1300

    first draft of "operators" exercises

---
+++ 02-operators/o3.py
@@ -0,0 +1,9 @@
+
+values = [1, 2, 3, 4, 5]
+
+# You can combine Boolean checks with a lowercase "and"
+statement = (1 in values) and (5000 > 500)
+print(
+    f"Both of these are {statement}: 1 is in values, and 5000 is more than 500.")
+
+# The parentheses on line 5 aren't needed; they were just there for clarity.
---
+++ 02-operators/o5.py
@@ -0,0 +1,7 @@
+
+values = [1, 2, 3, 4, 5]
+
+# Only when both parts are False does the expression evaluate to False
+
+statement = 6 in values or 7 in values
+print(statement)
