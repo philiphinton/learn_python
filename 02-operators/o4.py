9a2b67f434558c982e6e1f809bcf5ade72b518db
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 10:46:17 +1300

    update explanation

--- 02-operators/o4.py
+++ 02-operators/o4.py
@@ -7,4 +7,6 @@ values = [1, 2, 3, 4, 5]
 statement = 1 in values or 6 in values
 print(f"{statement}: either 1 or 6 is in 'values'.")
 
-# Note that you can't write "1 or 6 in values"; you have to do 2 whole checks.
+# Note that you can't write something like:
+#     statement = 1 or 6 in values
+# You have to do 2 whole checks
