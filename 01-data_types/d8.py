4d2a2af9073e3cb08c3f7c43001814ea0c1ecb90
hughlilly <hughlilly@users.noreply.github.com>
2021-12-15 19:46:54 +1300

    add note on other operators

--- 01-data_types/d8.py
+++ 01-data_types/d8.py
@@ -8,3 +8,6 @@ sum = x+y
 difference = x-y
 print(f"{x} plus {y} is {sum}.")
 print(f"{x} minus {y} is {difference}.")
+
+# There are other operators, such as "*" for multiplication,
+# "/" for division, and "%" for modulus.
