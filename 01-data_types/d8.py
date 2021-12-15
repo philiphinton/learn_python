60e162632db8b33aa00e321a523b703d700f7af9
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 10:40:39 +1300

    add more comments

--- 01-data_types/d8.py
+++ 01-data_types/d8.py
@@ -1,11 +1,13 @@
 # Numbers
 
-# Integers
+# Define two integers
 x = 12
 y = 8
 
+# Define two variables that will hold the result of these equations
 sum = x+y
 difference = x-y
+
 print(f"{x} plus {y} is {sum}.")
 print(f"{x} minus {y} is {difference}.")
 
