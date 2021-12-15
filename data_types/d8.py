22af1dce9a930d86a8bd333540a3bcf9bb27f113
hughlilly <hughlilly@users.noreply.github.com>
2021-12-15 18:26:53 +1300

    data types: numbers

---
+++ data_types/d8.py
@@ -0,0 +1,10 @@
+# Numbers
+
+# Integers
+x = 12
+y = 8
+
+sum = x+y
+difference = x-y
+print(f"{x} plus {y} is {sum}.")
+print(f"{x} minus {y} is {difference}.")
---
+++ data_types/d9.py
@@ -0,0 +1,11 @@
+# Numbers II: Floating-point numbers
+
+# Define Π to five decimal places
+pi_5 = 3.14159
+print(pi_5)
+
+# When printing floats, you can stipulate how they should appear
+# using ":.000f", where "000" is the number of decimal places
+
+# Print the variable we defined to two decimal places
+print(f"{pi_5:.2f}")
