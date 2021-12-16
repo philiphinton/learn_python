3126bfdf54353a15cf295a90083e8e51cfff2a0c
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 13:19:35 +1300

    explain parentheses

--- 02-operators/o7.py
+++ 02-operators/o7.py
@@ -4,5 +4,9 @@
 x = 12
 y = 8
 
-statement = (x + y == 20)
+statement = ((x + y) == 20)
+print(f"It is {statement} that {x} plus {y} is 20.")
+
+# Note that, again, the parentheses on line 7 don't need to be there
+statement = x + y == 20
 print(f"It is {statement} that {x} plus {y} is 20.")
