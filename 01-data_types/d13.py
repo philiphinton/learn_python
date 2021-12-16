e3f239aa2a57a3d904c807cf0b335173a8d59dae
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 17:21:18 +1300

    dict: + ex. of keys(), values(), & items() methods

---
+++ 01-data_types/d13.py
@@ -0,0 +1,16 @@
+
+shopping_list = {
+    'Tomatoes': 6,
+    'Bananas': 5,
+    'Crackers': 2,
+    'Sugar': 1,
+    'Icecream': 1,
+    'Bread': 3,
+    'Chocolate': 2
+}
+# Just the keys
+print(shopping_list.keys())
+# Just the values
+# print(shopping_list.values())
+# Both keys and values
+# print(shopping_list.items())
