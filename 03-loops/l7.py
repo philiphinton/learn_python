61671855a284e027b3c1b8fef4655aefe9481998
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 19:18:38 +1300

    complete `set`explanation

--- 03-loops/l7.py
+++ 03-loops/l7.py
@@ -1,8 +1,14 @@
 
 # There is another kind of object we haven't looked at yet: the "set"
-# A set in Python is
+# Sets contain only unique values, so you can loop through a list that
+# contains duplicates (like two "Bananas" here) and arrive at unique values
 
 shopping_list = ["Tomatoes", "Bananas", "Crackers",
                  "Sugar", "Icecream", "Bread", "Bananas", "Chocolate"]
 
-shopping_list.sort()
+unique_shopping_list = set()
+
+for item in shopping_list:
+    unique_shopping_list.add(item)
+
+print(unique_shopping_list)
