6ad47b644f33cb496a3cde575f685488c99f4e42
hughlilly <hughlilly@users.noreply.github.com>
2021-12-16 17:12:05 +1300

    expand context and explanation of functions

--- 04-functions/f1.py
+++ 04-functions/f1.py
@@ -1,6 +1,10 @@
 
-# Addition-only (note two blank lines after definition)
+# Let's make a calculator
 
+# Function definitions start with the 'def' keyword,
+# and are followed by two blank lines
+
+# First example: addition-only
 def add(x, y):
     sum = x + y
     return sum
@@ -8,3 +12,7 @@ def add(x, y):
 
 result = add(10, 20)
 print(f"The sum of 10 and 20 is {result}.")
+
+# Generally speaking, because the interpreter reads files from top to bottom,
+# calls to functions must come after their definitions.
+# Which is to say, you wouldn't have been able to call add() on line 1.
