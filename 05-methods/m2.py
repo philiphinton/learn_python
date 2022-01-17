0444ec66cddfc71a8f53fe1104e5ab131a02485d
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-01-17 14:37:50 +1300

    add explanation of catch-all (`*`) in imports

--- 05-methods/m2.py
+++ 05-methods/m2.py
@@ -10,3 +10,9 @@ today = date.today()
 
 long_format_date = today.strftime("%B %d, %Y")
 print(f"Today is {long_format_date}.")
+
+
+# There is also a catch-all operator, but its use is discouraged:
+#  from datetime import *
+# and as far as I can tell it doesn't do anything different to using
+# "import datetime", which also imports everything in that module.
