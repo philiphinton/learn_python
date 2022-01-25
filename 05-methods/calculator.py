a0ce0424e21ef583b4e80ba836df1479bff06042
hughlilly <hughlilly@users.noreply.github.com>
2022-01-25 16:20:27 +1300

    add module-level docstring
    See https://numpydoc.readthedocs.io/en/latest/format.html#returns

--- 05-methods/calculator.py
+++ 05-methods/calculator.py
@@ -1,6 +1,9 @@
+"""A simple calculator that can perform addition and subtraction.
+"""
+
 
 def calculate(x: int, y: int = 1, *, subtract: bool = False) -> int:
-    """Calculates the sum (or difference) of two numbers.
+    """Calculate sum (or difference) of two numbers.
 
     Parameters:
     `x` : int, required
@@ -9,6 +12,10 @@ def calculate(x: int, y: int = 1, *, subtract: bool = False) -> int:
         The second number (default is 1)
     `subtraction`: bool, optional
         Whether to perform subtraction. Default is False.
+
+    Returns:
+    int
+        The result of the operation on `x` and `y`
     """
 
     return x - y if subtract else x + y
