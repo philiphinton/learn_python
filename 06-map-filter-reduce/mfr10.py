fb32f2361ea766becd0bdfeb6013b59f22e0cab9
hughlilly <hughlilly@users.noreply.github.com>
2022-01-27 10:42:57 +1300

    ren variables

--- 06-map-filter-reduce/mfr10.py
+++ 06-map-filter-reduce/mfr10.py
@@ -12,10 +12,10 @@ print(f"The bill for this supermarket trip is ${total:.2f}", end="")
 items_on_special = filter(lambda item: item.on_special, shopping_list)
 
 # Create a list of prices and use `reduce()` to add them up
-prices_of_items_on_special = list(item.price for item in items_on_special)
-specials_only_total = reduce(lambda x, y: x + y, prices_of_items_on_special)
+specials_prices = list(item.price for item in items_on_special)
+specials_total = reduce(lambda x, y: x + y, specials_prices)
 
-savings = total - specials_only_total
+savings = total - specials_total
 
 print(" -- but if you just bought things that were on special, it'd be ", end="")
-print(f"${specials_only_total:.2f} (saving you ${savings:.2f}).")
+print(f"${specials_total:.2f} (saving you ${savings:.2f}).")
