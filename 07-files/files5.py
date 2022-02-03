bd3ea638dbb15120e3f540942167ffe156d61ad4
hughlilly <hughlilly@users.noreply.github.com>
2022-02-03 18:24:24 +1300

    update Hamlet exercise

--- 07-files/files5.py
+++ 07-files/files5.py
@@ -4,8 +4,8 @@
 # Define file path with an "r"-string
 filepath = r'07-files/soliloquy.txt'
 
-new_lines = ["To sleep, perchance to Dream; aye, there's the rub,",
-             "For in that sleep of death, what dreams may come,",
+new_lines = ["To sleep, perchance to dream; ay, there's the rub;",
+             "For in that sleep of death what dreams may come",
              "When we have shuffled off this mortal coil,",
              "Must give us pause."]
 
@@ -18,12 +18,16 @@ with open(filepath, mode='r') as reader:
 
 # The line count will be the position given by `enumerate()`, plus 1
 num_lines = position + 1
-print(f"File is {num_lines} lines long.")
+print(f"\nFile is {num_lines} lines long.\n")
+
+print("*" * 10)
+print("Hamlet, Act III, scene I")
+print("*" * 10 + '\n')
 
 # Open in append mode ('a')
 with open(filepath, mode='a') as writer:
 
-    # Write each line in the list to the file, and add a newline character;
+    # Append each line in the list to the file, and add a newline character;
     # without it, all the new lines would have been concatenated.
     for line in new_lines:
         writer.write(line + '\n')
@@ -31,6 +35,6 @@ with open(filepath, mode='a') as writer:
 # Print the newly added-to file, with line numbers.
 with open(filepath, mode='r') as reader:
     for line_num, line in enumerate(reader, start=1):
-        print(line_num, line)
+        print(line_num, line, end='')
 
-print(f"Wrote {len(new_lines)} extra lines.")
+print(f"\nWrote {len(new_lines)} extra lines.")
