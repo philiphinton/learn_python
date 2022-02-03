c5cda640f495c8d48e5835eb618d338f93981720
hughlilly <hughlilly@users.noreply.github.com>
2022-02-04 12:11:58 +1300

    csv comparison exercise

--- 07-files/files7.py
+++ 07-files/files7.py
@@ -1,2 +1,29 @@
 
-# Compare CSVs
+# Compare CSVs: https://stackoverflow.com/a/49444878/10267529
+
+import csv
+from pathlib import Path
+
+before_file = Path('./data/ALA_before.csv')
+after_file = Path('./data/ALA_after.csv')
+outfile = Path('./data/ALA_diff.csv')
+
+before = set(
+    map(
+        tuple,
+        csv.reader(open(before_file))
+    )
+)
+
+after = set(map(tuple, csv.reader(open(after_file))))
+
+differences = before ^ after
+
+output = csv.writer(open(outfile, mode='w'))
+
+compared = sorted(differences, key=lambda x: x[0], reverse=True)
+
+for iter, row in enumerate():
+    output.writerow(row)
+
+print(f"Wrote {iter} rows to output file.")
